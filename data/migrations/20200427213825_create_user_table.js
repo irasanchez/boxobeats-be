81bd27b57d58acc7176855150f1a3465193e8d15
Ira <ira.d.sanchez@gmail.com>
2020-04-27 22:01:28 -0700

    Add knex and migration

---
+++ data/migrations/20200427213825_create_user_table.js
@@ -0,0 +1,12 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("user", (tbl) => {
+    tbl.uuid("id");
+    tbl.text("stage_name", 128).unique().notNullable();
+    tbl.text("password", 255).notNullable();
+    tbl.text("email", 128).unique().notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("user");
+};
