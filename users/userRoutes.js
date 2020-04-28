942381b2b07d5e1bec50bc8956b26f3bdde7c3ce
Ira <ira.d.sanchez@gmail.com>
2020-04-27 22:51:14 -0700

    Cleanup

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const Users = require("./userModel");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).
+  res.status(200);
 });
 
 module.exports = router;
