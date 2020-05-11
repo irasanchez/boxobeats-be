22069cfed697ea1e2155f7020bc243d48778d28f
Ira <ira.d.sanchez@gmail.com>
2020-05-11 07:08:23 -0700

    Add dotenv and clean up server/index

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const protected = require("../auth/authMiddleware");
+const checkCredentials = require("../auth/authMiddleware");
 const soundRoutes = require("../sounds/soundRoutes.js");
 const userRoutes = require("../users/userRoutes.js");
 const authRoutes = require("../auth/authRoutes.js");
@@ -13,11 +13,9 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/api/auth", authRoutes);
-server.use("/api/user", protected, userRoutes);
-server.use("/api/sounds", soundRoutes);
+server.use("/api/user", checkCredentials, userRoutes);
+server.use("/api/sounds", checkCredentials, soundRoutes);
 
 server.use("/", (req, res) => res.send("API is up and running! 😊"));
-// watch for connections on port 5000
-server.listen(5000, () =>
-  console.log("Server running on http://localhost:5000")
-);
+
+module.exports = server;
